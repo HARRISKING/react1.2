2b3e921b808dab09e26b7c6b3a18097fe1cc2c69
HARRISKING <llc13588440410@outlook.com>
2017-09-29 14:40:32 +0800

    引入jQuery

--- webpack-demo/app/index.js
+++ webpack-demo/app/index.js
@@ -1,9 +1,9 @@
 import  _ from 'lodash'; 
+import $ from 'jquery';
 
 function component(){
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['hello','webpack'],' ');
-    return element;
+    var element = $('<div></div>').html(_.join(['hello','harrisking'],' '));
+    return element.get(0);
 }
 
 document.body.appendChild(component());
