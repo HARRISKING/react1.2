f577c37158fff3175bace301aef64051de5f4610
HARRISKING <llc13588440410@outlook.com>
2017-10-09 10:25:59 +0800

    new demo

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <h1>之前出现错误的原因是目录出现错误！！！</h1>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
