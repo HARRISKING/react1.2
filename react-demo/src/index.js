080d543b6868ff7868fc50d676bd9c8988a6b36d
HARRISKING <llc13588440410@outlook.com>
2017-10-13 15:13:29 +0800

    样式功能基本完成

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -8,11 +8,15 @@ import App from './App';
 
 
     ReactDOM.render(
-      <div className="quadrant-all">
-        <App title="重要但不紧急" />
-        <App title="重要又急迫" />
-        <App title="紧急但不重要" />
-        <App title="不紧急也不重要" />
+      <div>
+        <h1 className='allTitle'>四象限工作法</h1>
+        <div className="quadrant-all">
+          <App title="重要但不紧急" />
+          <App title="重要又急迫" />
+          <App title="紧急但不重要" />
+          <App title="不紧急也不重要" />
+        </div>
+        <h3 className='footerTitle'>本程序归HARRISKING所有，侵权必究！</h3>
       </div>,
       document.getElementById('root')
     );
