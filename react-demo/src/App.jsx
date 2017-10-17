2743883e4af4fbbedbb806823e6b77c2e369adad
HARRISKING <llc13588440410@outlook.com>
2017-10-17 20:42:40 +0800

    install leancloud

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -6,6 +6,23 @@ import './reset.css';
 import './App.css';
 import * as localStore from './localStore';
 
+
+var APP_ID = 'uVUdc1k6tUjkG63azsIyueoX-gzGzoHsz';
+var APP_KEY = '6RRok5ayAPNiOo2ohewr5J1x';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!');
+})
+
 class App extends React.Component{
 
     constructor(props){
