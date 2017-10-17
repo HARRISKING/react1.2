45a4223688e615f4ed086fd47e35c7597ac57d10
HARRISKING <llc13588440410@outlook.com>
2017-10-17 20:53:12 +0800

    新增le'ane新增leanCloude新增le'anCloude新增leanCloud.jsx

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -6,22 +6,17 @@ import './reset.css';
 import './App.css';
 import * as localStore from './localStore';
 
+import AV from './leanCloud'
 
-var APP_ID = 'uVUdc1k6tUjkG63azsIyueoX-gzGzoHsz';
-var APP_KEY = '6RRok5ayAPNiOo2ohewr5J1x';
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
 
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//   words: 'harriskingsdfdsfdsfds!'
+// }).then(function(object) {
+//   alert('LeanCloud Rocks!');
+// })
 
 class App extends React.Component{
 
---
+++ react-demo/src/leanCloud.jsx
@@ -0,0 +1,12 @@
+import AV from 'leancloud-storage';
+
+
+var APP_ID = 'uVUdc1k6tUjkG63azsIyueoX-gzGzoHsz';
+var APP_KEY = '6RRok5ayAPNiOo2ohewr5J1x';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV;
