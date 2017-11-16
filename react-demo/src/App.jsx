364cb3ddec6b575a44292a5da0fc4cdc62a1d038
HARRISKING <llc13588440410@outlook.com>
2017-11-16 13:40:19 +0800

    delete localStore

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -4,56 +4,12 @@ import TodoItems from './TodoItems';
 import 'normalize.css';
 import './reset.css';
 import './App.css';
-import * as localStore from './localStore';
+// import * as localStore from './localStore';
 
 import AV from './leanCloud'
 
 
 
-// var TestObject = AV.Object.extend('TestObject');
-// var testObject = new TestObject();
-// testObject.save({
-//   words: 'harriskingsdfdsfdsfds!'
-// }).then(function(object) {
-//   alert('LeanCloud Rocks!');
-// })
-
-
-
-// var TodoFolder = AV.Object.extend('TodoFolder');
-// var todoFolder =  new TodoFolder();
-
-// todoFolder.set('name','工作');
-// todoFolder.set('priority','1');
-// todoFolder.save().then(function(todo){
-//     console.log('objectID is'+ todo.id);
-// },function (error){
-//     console.error(error);
-// });
-
-
-var TestObject = AV.Object.extend('TestObject');
-
-var number = 34523423423;
-var strings = '我的随手输入的数字是'+number;
-var date = new Date();
-var arr = [strings,number];
-var object = {
-    number:number,
-    str : strings,
-    date : date
-}
-var testObject = new TestObject();
-testObject.set('testNumber',number);
-testObject.set('testString',strings);
-testObject.set('testDate',date);
-testObject.set('testObject',object);
-
-testObject.save().then(function(testObject){
-    console.log("success")
-},function(error){
-    console.log(error)
-})
 
 
 
@@ -67,13 +23,12 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            todoList:localStore.load(this.props.idx) || [],
+            todoList:[],
             newTodo:''
         }
     }
 
     render(){
-
         let todos = this.state.todoList.filter((item)=>!item.deleted).map((item,index)=>{
             return ( 
                <li key={index}>
